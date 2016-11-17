1a332f3ec73792e98fd30e1cb1d6b2f3b2cb33f2
rijpma <auke.rijpma@gmail.com>
2016-11-17 23:21:07 +0100

    fix map borders and wrong vrb

--- eanm_analysis.R
+++ eanm_analysis.R
@@ -14,6 +14,8 @@ library("countrycode")
 
 oecdregions = read.csv("/Users/auke/Dropbox/cliodata/oecdregions.csv")
 
+coast110 = maptools::readShapeSpatial("~/downloads/data/ne_110m_coastline/ne_110m_coastline.shp", proj4string=wgs)
+
 mdn = maptools::readShapeSpatial("matching/dat/mdn_withtrielevneo.shp")
 dist2coast = read.csv('matching/dat/results_50m.csv')
 mdn@data[c('dist2coast', 'lon_coast', 'lat_coast', 'coast_id')] = dist2coast[, -1]
@@ -104,64 +106,82 @@ mdn@data = factor2char(mdn@data)
 mdn_a = aggregate(mdn[, !sapply(mdn@data, is.character)], by=list(mdn$FeatureID), FUN=function(x) ifelse(all(is.na(x)), NA, mean(x, na.rm=T)))
 
 # no work atm
-# twowaycol = RColorBrewer::brewer.pal(3, 'Set3')[1:2]
-# for (vrb in vrbs){
-#     png(paste0('matching/fig/fvrbs_by_eth', vrb, '.png'), 
-#         width=1080, height=600)
-#     plot(mdn_a, col=to_col(mdn_a@data[, vrb]), lwd=0.2)
-#     add_legend(mdn_a@data[, vrb])
-#     title(main=fullvrbs[vrb])
-#     dev.off()
-# }
+twowaycol = RColorBrewer::brewer.pal(3, 'Set3')[1:2]
+for (vrb in vrbs){
+    png(paste0('matching/fig/fvrbs_by_eth', vrb, '.png'), 
+        width=1080, height=600)
+    plot(mdn_a, col=to_col(mdn_a@data[, vrb]), lwd=0.05)
+    plot(coast110, add=T, lwd=0.5)
+    add_legend(mdn_a@data[, vrb])
+    title(main=fullvrbs[vrb])
+    dev.off()
+}
 
 # x = unique(data.frame(mdn_a@data[, vrb], to_col(mdn_a@data[, vrb])))
 # x = x[order(x[, 1]), ]
 # plot(x[, 1], 1:13, col=x[,2])
 
-# commented out for speed
-# png('matching/fig/fcowa_fa.png', width=1080, height=600)
-# plot(mdn_a, col=to_col(mdn_a@data$fa), lwd=0.2, main='Fam. constraints on women’s agency: FA')
-# add_legend(x=mdn_a@data$fa)
-# dev.off()
+png('matching/fig/fcowa_fa.png', width=1080, height=600)
+plot(mdn_a, col=to_col(mdn_a@data$fa), lwd=0.05, 
+    main='Fam. constraints on women’s agency: FA')
+plot(coast110, add=T, lwd=0.5)
+add_legend(x=mdn_a@data$fa)
+dev.off()
 
-# png('matching/fig/fcowa_fapolych.png', width=1080, height=600)
-# plot(mdn_a, col=to_col(mdn_a@data$fapoly), lwd=0.2, main='Fam. constraints on women’s agency: polych. FA')
-# add_legend(mdn_a@data$fapoly)
-# dev.off()
+png('matching/fig/fcowa_fapolych.png', width=1080, height=600)
+plot(mdn_a, col=to_col(mdn_a@data$fapoly), lwd=0.05, 
+    main='Fam. constraints on women’s agency: polych. FA')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn_a@data$fapoly)
+dev.off()
 
-# png('matching/fig/fcowa_prcomp.png', width=1080, height=600)
-# plot(mdn_a, col=to_col(mdn_a@data$pc1), lwd=0.2, main='Fam. constraints on women’s agency: 1st principal component')
-# add_legend(mdn_a@data$pc1)
-# dev.off()
+png('matching/fig/fcowa_prcomp.png', width=1080, height=600)
+plot(mdn_a, col=to_col(mdn_a@data$pc1), lwd=0.05, 
+    main='Fam. constraints on women’s agency: 1st principal component')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn_a@data$pc1)
+dev.off()
 
-# png('matching/fig/dist2neo_crow.png', width=1080, height=600)
-# plot(mdn, col=to_col(mdn@data$dist2neo_c), lwd=0.2, main='Distance to nearest neolitihic revolution site, “as crow flies”')
-# add_legend(mdn@data$dist2neo_c)
-# dev.off()
+png('matching/fig/dist2neo_crow.png', width=1080, height=600)
+plot(mdn, col=to_col(mdn@data$dist2neo_c), lwd=0.05, 
+    main='Distance to nearest neolitihic revolution site, “as crow flies”')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn@data$dist2neo_c)
+dev.off()
 
-# png('matching/fig/dist2neo_land.png', width=1080, height=600)
-# plot(mdn[!is.infinite(mdn@data$dist2neo_l), ], col=to_col(mdn@data$dist2neo_l[!is.infinite(mdn@data$dist2neo_l)]), lwd=0.2, main='Distance to nearest neolitihic revolution site, “as wolf runs”')
-# add_legend(mdn@data$dist2neo_l[!is.infinite(mdn@data$dist2neo_l)])
-# dev.off()
+png('matching/fig/dist2neo_land.png', width=1080, height=600)
+plot(mdn[!is.infinite(mdn@data$dist2neo_l), ], col=to_col(mdn@data$dist2neo_l[!is.infinite(mdn@data$dist2neo_l)]), lwd=0.05, 
+    main='Distance to nearest neolitihic revolution site, “as wolf runs”')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn@data$dist2neo_l[!is.infinite(mdn@data$dist2neo_l)])
+dev.off()
 
-# png('matching/fig/neoregion_land.png', width=1080, height=600)
-# plot(mdn, col=as.factor(mdn@data$neo_by_lan.1), lwd=0.2, main='Neolothic region (by land)')
-# dev.off()
+png('matching/fig/neoregion_land.png', width=1080, height=600)
+plot(mdn, col=as.factor(mdn@data$neo_by_lan.1), lwd=0.05, 
+    main='Neolothic region (by land)')
+plot(coast110, add=T, lwd=0.5)
+dev.off()
 
-# png('matching/fig/dist2coast.png', width=1080, height=600)
-# plot(mdn, col=to_col(mdn@data$dist2coast), lwd=0.2, main='Distance to coast, “as crow flies” from poly. centre')
-# add_legend(mdn@data$dist2coast)
-# dev.off()
+png('matching/fig/dist2coast.png', width=1080, height=600)
+plot(mdn, col=to_col(mdn@data$dist2coast), lwd=0.05, 
+    main='Distance to coast, “as crow flies” from poly. centre')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn@data$dist2coast)
+dev.off()
 
-# png('matching/fig/elevation.png', width=1080, height=600)
-# plot(mdn, col=to_col(mdn@data$elev), lwd=0.2, main='Mean elevation (m)')
-# add_legend(mdn@data$elev)
-# dev.off()
+png('matching/fig/elevation.png', width=1080, height=600)
+plot(mdn, col=to_col(mdn@data$elev), lwd=0.05, 
+    main='Mean elevation (m)')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn@data$elev)
+dev.off()
 
-# png('matching/fig/ruggedness.png', width=1080, height=600)
-# plot(mdn, col=to_col(mdn@data$elev), lwd=0.2, main='Mean ruggedness (mean of the absolute differences between cell its 8 neighbours')
-# add_legend(mdn@data$elev)
-# dev.off()
+png('matching/fig/ruggedness.png', width=1080, height=600)
+plot(mdn, col=to_col(sqrt(mdn@data$tri)), lwd=0.05, 
+    main='Mean ruggedness (mean absolute differences between cell\'s 8 neighbours)')
+plot(coast110, add=T, lwd=0.5)
+add_legend(mdn@data$tri)
+dev.off()
 
 mdn@data$dist2coast_trunc = ifelse(mdn@data$dist2coast < 0, 0, mdn@data$dist2coast)
 mdn@data$iso3c = countrycode::countrycode(mdn@data$COW, 'cown', 'iso3c')
